695ad055d7c67a920dd0d5cf3bf7cdc2e327f3db
origin <@https://github.com/CoralCeleste28/My-First-Repo>
2025-09-24 14:06:43 -0500

    Save question 4

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,37 @@
+public class MathApp {
+    public static void main(String[] args) {
+       //Question 1
+        double bobSalary = 30.00;
+        double garySalary = 27.00;
+        double highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("Question 1");
+        System.out.println("The highest salary is " + highestSalary + "\n");
+
+        //Question 2
+        double carPrice = 15000.00;
+        double truckPrice = 25000.00;
+        System.out.println("Question 2");
+        System.out.println("Car Price: " + carPrice);
+        System.out.println("Truck Price: " + truckPrice + "\n");
+
+        //Question 3
+        double radius = 7.25;
+        double area = Math.PI * radius * radius;
+        System.out.println("Question 3");
+        System.out.println("The area of the circle is " + area);
+
+        //Question 4
+        int number = 25;
+        Math.sqrt(25);
+        System.out.println("The square root of " + number + " is " +  );
+
+
+
+
+    }
+
+
+
+
+
+}
